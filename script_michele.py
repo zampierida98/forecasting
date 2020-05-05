8ff32f31fce5f2693d00f310a0ea644748d59576
dallachiaram <dallachiaram@gmail.com>
2020-05-05 14:03:11 +0200

    aggiunto i gradi di forza e miglioramento dello scipt_michele.py

--- script_michele.py
+++ script_michele.py
@@ -5,10 +5,11 @@ Scipt di forecasting dati di Albignasego
 import matplotlib.pylab as plt
 import numpy as np
 import pandas as pd
-import statsmodels.api as sm
-from statsmodels.tsa.stattools import acf, pacf
+import datetime 
 from statsmodels.tsa.arima_model import ARIMA
-from statsmodels.tsa.stattools import adfuller
+from statsmodels.tsa.seasonal import seasonal_decompose
+from statsmodels.tsa.stattools import acf, pacf, adfuller
+
 
 def timeplot(ts, label, linestyle='-'):
     '''
@@ -35,15 +36,18 @@ def timeplot(ts, label, linestyle='-'):
 
 def plotAcf(ts, isPacf=False, both=False, lags=40):
     '''
-    Funzione che realizza un plot della funzione acf/pacf (o entrambe) 
-    di una serie temporale
+    Funzione che realizza un plot della acf/pacf (o entrambe) 
+    data una serie temporale
     Parameters
     ----------
-    ts : TYPE
-        DESCRIPTION.
-    isPacf : TYPE
-        DESCRIPTION.
-
+    ts : pandas.Series
+        Serie temporale
+    isPacf : bool
+        Se vogliamo invece di acf la pacf
+    both : bool
+        Se vogliamo entrambi acf e pacf in un unico grafico
+    lags : int
+        Quanti valori deve assumere k di autocor o p_autocor
     Returns
     -------
     None.
@@ -78,14 +82,16 @@ def test_stationarity(ts):
     https://www.performancetrading.it/Documents/PmEconometria/PmEq_Dickey_Fuller.htm
     Esegue un test di stazionarietà usando il test Dickey-Fuller. 
     Controlliamo se il valore di 'test statistic' 
-    è minore del valore critico 1%
+    è minore del valore critico 1% così da avere una buona certezza di stazionarietà
     Parameters
     ----------
-    ts : TYPE
+    ts : pandas.Series
         Serie temporale
 
-    Returns True se stazionaria, False non stazionaria
+    Returns
     -------
+    True se stazionaria, False non stazionaria
+    
     '''
     
     print('Risultati del test di Dickey-Fuller aumentato:')
@@ -100,6 +106,26 @@ def test_stationarity(ts):
     return dftest[0] < dftest[4]['1%']
 
 def p_QForArima(ts_diff, T, lags=40):
+    '''
+    È la funzione che determina a partire dalla funzione acf e pacf i valori di
+    p e q senza guardare i grafici.
+    Parameters
+    ----------
+    ts_diff : pandas.Series
+        Serie temporale differenziata
+    T : int
+        Numero di osservazioni della serie 
+    lags : int, optional
+        Quanti valori deve assumere k di autocor e p_autocor. The default is 40.
+
+    Returns
+    -------
+    p : int
+        La p di arima
+    q : int
+        la q di arima
+
+    '''
     p = 0
     q = 0
     auto_cor = acf(ts_diff, nlags=lags)
@@ -137,103 +163,137 @@ def find_best_model(ts, d=0, max_p=5, max_q=5):
     -------
     (p,d, result_model) : (int, int, ARIMAResults)
     '''
-    min_aic = 2**32; p = 0; q = 0    
-    best_model = None
+    min_aic = 2**32; p = 0; q = 0
+    result_model = None
     for i in range(0, max_p):
         for j in range(0, max_q):
-            model = ARIMA(ts, order=(i, d, j)).fit(disp=-1)
-            if (model.aic < min_aic):
-                p=i
-                q=j
-                min_aic=model.aic
-                result_model=model
-
+            try:
+                model = ARIMA(ts, order=(i, d, j)).fit(disp=-1)
+                if (model.aic < min_aic):
+                    p=i
+                    q=j
+                    min_aic=model.aic
+                    result_model=model
+            except:
+                continue
     return (p,q, result_model)
+
+def strength_seasonal_trend(ts):
+    '''
+    La funzione calcola, sfruttando la decomposizione delle ts, la forza della
+    stagionalità e del trend
+    
+    Parameters
+    ----------
+    ts : pandas.Series
+        Serie temporale
+
+    Returns
+    -------
+    strength_seasonal : float
+        Grado della stagionalità
+    strength_seasonal_trend : float
+        Grado del trend
+
+    '''
+    decomposition = seasonal_decompose(ts)
+    
+    trend = decomposition.trend
+    seasonal = decomposition.seasonal
+    residual = decomposition.resid
+    
+    strength_trend = max(0, 1 - residual.var()/(trend + residual).var())
+    strength_seasonal = max(0, 1 - residual.var()/(seasonal + residual).var())
+    
+    return (strength_seasonal, strength_trend)
+
 if __name__ == "__main__":
     # COLONNE #,MAGLIE,CAMICIE,GONNE,PANTALONI,VESTITI,GIACCHE
     
     #Whole period.csv
-    dateparser = lambda dates: pd.datetime.strptime(dates, '%Y-%m-%d')
+    dateparser = lambda dates: datetime.datetime.strptime(dates, '%Y-%m-%d')
     dataframe = pd.read_csv('Dati_Albignasego/Whole period.csv',index_col = 0, date_parser=dateparser)
     
     # Analizziamo i dati graficamente
-    '''
     for column in dataframe:
-        timeplot(ts=dataframe[column], label=column)'''    
-    # Commentato perchè impiega davvero tanto tempo
-    
-    # Lavoriamo intanto con le MAGLIE
-    maglie = dataframe['MAGLIE']
-    
-    # Commento il training set dell'80% sui dati
-    '''
-    maglie = maglie[pd.date_range(start=dataframe.index[0], 
-                                    end=maglie.index[int(len(maglie) * 0.8)] , freq='D')]
-    '''
-   
-    # Cerchiamo di capire se la serie è stagionale e/o presenta trend dalla funzione acf
-    plotAcf(maglie, both=True)
-    
-    # Dal grafico della funzione ACF osserviamo che maglie presenta sia un trend
-    # sia una stagionalità
-    
-    if (test_stationarity(maglie)):
-        print('La serie temporale %s è stazionaria'%'MAGLIE')
-    else:
-        print('La serie temporale %s NON è stazionaria'%'MAGLIE')
-    
-    # Visto che la serie delle MAGLIE è stazionaria allora allora per ARIMA d=0
-    # Determiniamo dunque il valore di p e q con le funzioni ACF e PACF su la serie differenziata
-    # p = il valore di k dove la pacf interseca con 1.96/sqrt(T)
-    # q = il valore di k dove la acf interseca con 1.96/sqrt(T)
-    maglie_diff=maglie.diff()
-    maglie_diff.dropna(inplace=True)
-    plotAcf(maglie_diff, both=True)
-    
-    # Definiamo dunque il modello ARIMA    
-    p,q = p_QForArima(maglie_diff, len(maglie))
-    
-    model = ARIMA(maglie, order=(p, 0, q))
-    results_arima = model.fit(disp=-1)
-    
-    # Ricordo che essendo la serie stazionaria non devo fare le somme cumulative
-    arima_model = pd.Series(results_arima.fittedvalues, copy=True)
-    
-    # Plottamo grafico normale e arima_model
-    plt.figure(figsize=(40, 20), dpi=80)
-    plt.plot(maglie, color='black', label='MAGLIE')
-    plt.plot(arima_model, color='red', label='Modello ARIMA(' + str(p) + ',0,' + str(q) +')')
-    plt.show()
-    
-    # Individuiamo il miglior modello ARIMA che approssima meglio la serie temporale
-    best_p, best_q, best_result_model = find_best_model(maglie)
-    #best_p, best_q, best_result_model = (4,4,ARIMA(maglie, order=(4, 0, 4)).fit(disp=-1))
-    
-    # Mettiamo in confronto i due modelli
-    plt.figure(figsize=(40, 20), dpi=80)
-    plt.subplot(211)
-    plt.plot(maglie, label='MAGLIE', color='black')
-    plt.plot(arima_model,color='red', label='Modello ARIMA(' + str(p) + ',0,' + str(q) +')')
-    plt.legend(loc='best');
-    plt.subplot(212)
-    plt.plot(maglie, label='MAGLIE', color='black')
-    best_arima_model = pd.Series(best_result_model.fittedvalues, copy=True)
-    plt.plot(best_arima_model, color='green', label='Modello ARIMA(' + str(best_p) + ',0,' + str(best_q) +')')
-    plt.legend(loc='best');
-    plt.show()
-    
-    
-    # Usiamo il miglior modello per fare le previsioni
-    
-    h = 50  # orizzonte 
-    
-    previsione, _ ,intervallo = best_result_model.forecast(steps=h)
-    plt.figure(figsize=(40, 20), dpi=80)
-    plt.plot(best_arima_model, color="green", label='Modello ARIMA(' + str(best_p) + ',0,' + str(best_q) +')')
-    plt.plot(pd.date_range(start=maglie.index[len(maglie) - 1], periods=h , freq='D'), 
-             previsione, linestyle='--',color='red', label='Previsioni')
-    plt.plot(pd.date_range(start=maglie.index[len(maglie) - 1], periods=h , freq='D'), 
-             intervallo, linestyle='--', color='red')
-    plt.legend(loc='best');
-    plt.show()
-    
+        timeplot(ts=dataframe[column], label=column)
+        # Recuperiamo una serie temporale
+        serie = dataframe[column]
+        
+        # Commento il training set dell'80% sui dati
+        '''
+        serie = serie[pd.date_range(start=dataframe.index[0], 
+                                        end=serie.index[int(len(serie) * 0.8)] , freq='D')]
+        '''
+       
+        # Cerchiamo di capire se la serie è stagionale e/o presenta trend dalla funzione acf
+        plotAcf(serie, both=True)
+        
+        # Forza delle componenti stagionalità e trend
+        strength_s, strength_t = strength_seasonal_trend(serie)
+        print('Forza dei gradi stagionalita (%s), trend(%s)'%(str(strength_s), str(strength_t)))
+        
+        # Dal grafico della funzione ACF osserviamo che serie presenta sia un trend
+        # sia una stagionalità
+        isStationary = test_stationarity(serie)
+        
+        if (isStationary):
+            print('La serie temporale %s è stazionaria'%column)
+        else:
+            print('La serie temporale %s NON è stazionaria'%column)
+        
+        # Visto che la serie delle serie è stazionaria allora allora per ARIMA d=0
+        # Determiniamo dunque il valore di p e q con le funzioni ACF e PACF su la serie differenziata
+        # p = il valore di k dove la pacf interseca con 1.96/sqrt(T)
+        # q = il valore di k dove la acf interseca con 1.96/sqrt(T)
+        serie_diff=serie.diff()
+        serie_diff.dropna(inplace=True)
+        plotAcf(serie_diff, both=True)
+        
+        # Definiamo dunque il modello ARIMA    
+        p,q = p_QForArima(serie_diff, len(serie))
+        
+        model = ARIMA(serie, order=(p, int(not isStationary), q))
+        results_arima = model.fit(disp=-1)
+        
+        # Ricordo che essendo la serie stazionaria non devo fare le somme cumulative
+        arima_model = pd.Series(results_arima.fittedvalues, copy=True)
+        
+        # Plottamo grafico normale e arima_model
+        plt.figure(figsize=(40, 20), dpi=80)
+        plt.plot(serie, color='black', label=column)
+        plt.plot(arima_model, color='red', label='Modello ARIMA(' + str(p) + ',' + str(int(not isStationary)) + ',' + str(q) +')')
+        plt.legend(loc='best');
+        plt.show()
+        
+        # Individuiamo il miglior modello ARIMA che approssima meglio la serie temporale
+        best_p, best_q, best_result_model = find_best_model(serie, d=int(not isStationary))
+        #best_p, best_q, best_result_model = (4,4,ARIMA(serie, order=(4, 0, 4)).fit(disp=-1))
+        
+        # Mettiamo in confronto i due modelli
+        plt.figure(figsize=(40, 20), dpi=80)
+        plt.subplot(211)
+        plt.plot(serie, label=column, color='black')
+        plt.plot(arima_model,color='red', label='Modello ARIMA(' + str(p) + ',' + str(int(not isStationary)) + ',' + str(q) +')')
+        plt.legend(loc='best');
+        plt.subplot(212)
+        plt.plot(serie, label=column, color='black')
+        best_arima_model = pd.Series(best_result_model.fittedvalues, copy=True)
+        plt.plot(best_arima_model, color='green', label='Modello ARIMA(' + str(best_p) + ',' + str(int(not isStationary)) + ',' + str(best_q) +')')
+        plt.legend(loc='best');
+        plt.show()
+        
+        
+        # Usiamo il miglior modello per fare le previsioni
+        
+        h = 50  # orizzonte
+        
+        previsione, _ ,intervallo = best_result_model.forecast(steps=h)
+        plt.figure(figsize=(40, 20), dpi=80)
+        plt.plot(best_arima_model, color="green", label='Modello ARIMA(' + str(best_p) + ',0,' + str(best_q) +')')
+        plt.plot(pd.date_range(start=serie.index[len(serie) - 1], periods=h , freq='D'), 
+                 previsione, linestyle='--',color='red', label='Previsioni')
+        plt.plot(pd.date_range(start=serie.index[len(serie) - 1], periods=h , freq='D'), 
+                 intervallo, linestyle='--', color='red')
+        plt.legend(loc='best');
+        plt.show()
