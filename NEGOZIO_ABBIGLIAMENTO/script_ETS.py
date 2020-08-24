e26ce0bc970d70da46ca7ecd155a764cdc9c930b
ScodroS <40734838+ScodroS@users.noreply.github.com>
2020-08-24 16:12:19 +0200

    modifiche e aggiunte

--- NEGOZIO_ABBIGLIAMENTO/script_ETS.py
+++ NEGOZIO_ABBIGLIAMENTO/script_ETS.py
@@ -300,3 +300,23 @@ if __name__ == "__main__":
     print('MSE: %f' % mse)
     print("Calcoliamo  MAE=%.4f"%(sum(abs(errore))/len(errore)))
     
+"""
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from statsmodels.tsa.holtwinters import ExponentialSmoothing
+
+df = pd.read_csv('/home/ayhan/international-airline-passengers.csv', 
+                 parse_dates=['Month'], 
+                 index_col='Month'
+)
+df.index.freq = 'MS'
+train, test = df.iloc[:130, 0], df.iloc[130:, 0]
+model = ExponentialSmoothing(train, seasonal='mul', seasonal_periods=12).fit()
+pred = model.predict(start=test.index[0], end=test.index[-1])
+
+plt.plot(train.index, train, label='Train')
+plt.plot(test.index, test, label='Test')
+plt.plot(pred.index, pred, label='Holt-Winters')
+plt.legend(loc='best')
+"""
